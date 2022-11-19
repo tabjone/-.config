233f838260b3e387e264d91f5a736b40c17e3436
Trrn13P <tor-andreas_93@hotmail.com>
2022-11-19 11:24:32 +0100

    config

---
+++ nvim/init.lua
@@ -0,0 +1,54 @@
+require("tabjone.plugins-setup")
+require("tabjone.core.options")
+require("tabjone.core.keymaps")
+require("tabjone.core.colorscheme")
+--require("tabjone.plugins.comment")
+require("tabjone.plugins.nvim-tree")
+
+--[[
+Under here is the old stuff
+--]]
+
+
+vim.o.swapfile = false
+vim.o.undodir = '~/.config/nvim/undodir/'
+vim.o.undofile = true
+
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('i', '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u', opts)
+
+vim.keymap.set('n', '<esc><esc>', ':noh<CR><esc>', opts)
+
+vim.o.timeoutlen = 1000
+vim.o.ttimeoutlen = 0
+
+vim.o.wrap = false
+
+vim.cmd[[ autocmd BufNewFile,BufRead *.tex
+        \ set textwidth=79 |
+        \ setlocal spell spelllang=en ]]
+
+--[[
+vim.cmd[[
+	set number
+	set relativenumber
+	set autoindent
+	set mouse=a
+	colorscheme slate
+	syntax on
+	set nowrap
+]]
+
+--vim.o.tabstop = 4
+--vim.o.softtabstop = 4
+--vim.o.shiftwidth = 4
+--vim.o.expandtab = true
+--vim.o.smartindent = true
+
+
+
+
+ --]]
+
+
